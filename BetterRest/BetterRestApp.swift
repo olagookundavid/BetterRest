48f7f57de91534bb4c0998f6eb97eb7e4af7b5b6
David Olagookun <erijesudo@gmail.com>
2023-03-09 15:02:46 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by David OH on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
